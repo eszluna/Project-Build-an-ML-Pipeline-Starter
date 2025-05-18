a66c41670a6f54809196025a442d3bef36f5804f
eluna24 <eluna24@wgu.edu>
2025-05-17 17:58:32 -0700

    Fix path to local basic_cleaning step for mlflow.run

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def go(config: DictConfig):
 
         if "basic_cleaning" in active_steps:
             _ = mlflow.run(
-                os.path.join("src", "basic_cleaning"),
+                "./src/basic_cleaning"
                 "main",
                 env_manager="conda",
                 parameters={
@@ -64,7 +64,6 @@ def go(config: DictConfig):
                     "max_price": config["etl"]["max_price"],
                 },
             )
-
 
         if "data_check" in active_steps:
             ##################
