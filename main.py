e579c91a879bb33e09df2ef7d4e74b2c8bba694f
eluna24 <eluna24@wgu.edu>
2025-05-18 08:51:43 -0700

    Updated basic_cleaning code

--- main.py
+++ main.py
@@ -52,19 +52,17 @@ def go(config: DictConfig):
 
         if "basic_cleaning" in active_steps:
             _ = mlflow.run(
-                f"{config['main']['components_repository']}/get_data",
-                "main",
-                version='main',
-                env_manager="conda",
-                parameters={
-                    "input_artifact": "sample.csv:latest",
-                    "output_artifact": "clean_sample.csv",
-                    "output_type": "cleaned_data",
-                    "output_description": "Data with outliers and null values removed",
-                    "min_price": config["etl"]["min_price"],
-                    "max_price": config["etl"]["max_price"],
-                },
-            )
+            os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+            entry_point="main",
+            parameters={
+                "input_artifact": "sample.csv:latest",  
+                "output_artifact": "clean_sample.csv",
+                "output_type": "cleaned_data",
+                "output_description": "Cleaned dataset after basic preprocessing",
+                "min_price": config["etl"]["min_price"],
+                "max_price": config["etl"]["max_price"],
+            },
+        )
 
         if "data_check" in active_steps:
             ##################
